9d188b96b6ceaefe741605f27cd2d9885a478eec
elebeaudour <enora.le-beaudour@insa-lyon.fr>
2024-05-30 21:23:17 +0200

    Add files via upload

--- Model/Trip.py
+++ Model/Trip.py
@@ -1,13 +1,11 @@
 class Trip:
-    __slots__ = ["user_id", "country_id", "date"]
+    __slots__ = ["user_id", "departure_id", "destination_id", "date"]
 
-    def __init__(self, user_id, country_id, date):
+    def __init__(self, user_id, departure_id, destination_id, date):
         self.user_id = user_id
-        self.country_id = country_id
+        self.departure_id = departure_id
+        self.destination_id = destination_id
         self.date = date
-
-    def __str__(self):
-        return f"Trip from {self.departure} to {self.destination} on {self.date}."
     
     def distance(self):
         """
