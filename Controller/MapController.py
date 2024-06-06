90affb7a9cb283d694be226356b5ce4747bb2b21
elebeaudour <enora.le-beaudour@insa-lyon.fr>
2024-06-06 18:46:18 +0200

    Add files via upload

--- Controller/MapController.py
+++ Controller/MapController.py
@@ -1,5 +1,6 @@
 from Repository.TripRepository import get_all_trips_by_user_id
-from Repository.CountryRepository import get_country_name_by_id, get_description_by_name
+from Repository.CountryRepository import get_country_name_by_id, get_description_by_name, get_country_id_by_name
+from Repository.MonumentRepository import get_list_of_monuments_by_country_id
 from datetime import datetime
 
 def get_country_name(country_id):
@@ -12,7 +13,7 @@ def get_incoming_trips(user_id):
     reminders = []
     for trip in get_all_trips_by_user_id(user_id):
         today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-        if (trip.date - today).days <= 7 and (trip.date - today).days >= 0:
+        if (trip.departure_date - today).days <= 7 and (trip.departure_date - today).days >= 0:
             reminders.append(trip)
 
     return reminders
@@ -21,7 +22,7 @@ def get_all_countries_visited(user_id):
     countries = []
     for trip in get_all_trips_by_user_id(user_id):
         today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-        if trip.date < today:
+        if trip.departure_date < today:
             departure = get_country_name(trip.departure_id)
             destination = get_country_name(trip.destination_id)
             if departure not in countries:
@@ -31,11 +32,19 @@ def get_all_countries_visited(user_id):
 
     return countries
 
+def get_despcriptions_monuments(country_name):
+    country_id = get_country_id_by_name(country_name)
+    monuments_list = get_list_of_monuments_by_country_id(country_id)
+    text = "\n\n"
+    for monument in monuments_list:
+        text += monument.__str__() + "\n"
+    return text   
+
 def get_all_countries_to_visit(user_id):
     countries = []
     for trip in get_all_trips_by_user_id(user_id):
         today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-        if trip.date > today:
+        if trip.return_date > today:
             destination = get_country_name(trip.destination_id)
             if destination not in countries:
                 countries.append(destination)
--- Controller/TripController.py
+++ Controller/TripController.py
@@ -1,25 +1,99 @@
-from Repository.TripRepository import add_trip, modify_trip, delete_trip
-from Repository.CountryRepository import get_country_id_by_name
+from Repository.TripRepository import add_trip, modify_trip, delete_trip, get_all_trips_by_user_id
+from Repository.CountryRepository import get_country_id_by_name, get_all_countries_name, get_country_name_by_id
 from datetime import datetime
 
-def get_country_id_by_name(country_name):
+def add_new_trip(user_id, departure_country, destination_country, departure_date, return_date):
+    if departure_country == "" or destination_country == "" or departure_date == "" or return_date == "":
+        return False, "Please type all fields"
+    
+    if departure_country == destination_country:
+        return False, "Departure and destination countries must be different"
+    
+    if return_date < departure_date:
+        return False, "Return date must be after departure date"
+    
+    if departure_date < datetime.now().replace(hour=0, minute=0, second=0, microsecond=0):
+        return False, "Departure date must be in the future"
+    
+    for trip in get_all_trips_by_user_id(user_id): # Check if the departure date, return date have conflict with other trips
+        if (departure_date >= trip.departure_date and departure_date <= trip.return_date) \
+            or (return_date >= trip.departure_date and return_date <= trip.return_date) \
+            or (departure_date <= trip.departure_date and return_date >= trip.return_date):
+
+            return False, "You already have a trip planned at this date"
+    
+    departure_id = get_country_id_by_name(departure_country)
+    destination_id = get_country_id_by_name(destination_country)
+
+    return add_trip(user_id, departure_id, destination_id, departure_date, return_date), "Trip added successfully"
+
+def update_planned_trip(old_trip, new_departure_country, new_destination_country, new_departure_date, new_return_date):
+    if new_departure_country == "" or new_destination_country == "":
+        return False, "Please type all fields"
+    
+    if new_departure_country == new_destination_country:
+        return False, "Departure and destination countries must be different"
+    
+    if new_return_date < new_departure_date:
+        return False, "Return date must be after departure date"
+    
+    if new_departure_date < datetime.now().replace(hour=0, minute=0, second=0, microsecond=0):
+        return False, "Departure date must be in the future"
+    
+    for trip in get_all_trips_by_user_id(old_trip.user_id):
+        if trip.__eq__(old_trip):
+            continue
+        if (new_departure_date >= trip.departure_date and new_departure_date <= trip.return_date) \
+            or (new_return_date >= trip.departure_date and new_return_date <= trip.return_date) \
+            or (new_departure_date <= trip.departure_date and new_return_date >= trip.return_date):
+
+            return False, "You already have a trip planned at this date"
+        
+    departure_id = get_country_id_by_name(new_departure_country)
+    destination_id = get_country_id_by_name(new_destination_country)
+
+    return modify_trip(old_trip, departure_id, destination_id, new_departure_date, new_return_date), "Trip modified successfully"
+
+def delete_planned_trip(user_id, departure_country, destination_country, departure_date, return_date):
+    departure_id = get_country_id_by_name(departure_country)
+    destination_id = get_country_id_by_name(destination_country)
+
+    return delete_trip(user_id, departure_id, destination_id, departure_date, return_date)
+
+def get_all_past_trips(user_id):
+    all_trips = get_all_trips_by_user_id(user_id)
+    past_trips = []
+    today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+    for trip in all_trips:
+        if trip.departure_date < today:
+            past_trips.append(trip)
+
+    # Sort the list of past trips by departure date
+    past_trips.sort(key=lambda x: x.departure_date, reverse=True) # Reverse the list to have the most recent trip first
+            
+    return past_trips
+
+def get_all_upcoming_trips(user_id):
+    all_trips = get_all_trips_by_user_id(user_id)
+    upcoming_trips = []
+    today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+    for trip in all_trips:
+        if trip.departure_date >= today:
+            upcoming_trips.append(trip)
+
+    # Sort the list of upcoming trips by departure date
+    upcoming_trips.sort(key=lambda x: x.departure_date, reverse=False)
+            
+    return upcoming_trips
+
+def get_all_countries_without_chosen(country):
+    all_countries = get_all_countries_name()
+    if country is not None:
+        all_countries.remove(country)
+    return all_countries
+
+def get_country_name(country_id):
+    return get_country_name_by_id(country_id)
+
+def get_country_id(country_name):
     return get_country_id_by_name(country_name)
-
-def add_new_trip(user_id, departure_id, arrival_id, date_str):
-    # Check if the date is in the future
-    
-    date = datetime.strptime(date_str, "%d/%m/%Y")
-    if date < datetime.now().replace(hour=0, minute=0, second=0, microsecond=0):
-        return False
-    add_trip(user_id, departure_id, arrival_id, date)
-    return True
-
-def update_planned_trip(user_id, country_id, date, new_date):
-    # Check if the new date is in the future
-    if new_date < datetime.now().replace(hour=0, minute=0, second=0, microsecond=0):
-        return False
-    
-    return modify_trip(user_id, country_id, date, new_date)
-
-def delete_planned_trip(user_id, country_id, date):
-    return delete_trip(user_id, country_id, date)
